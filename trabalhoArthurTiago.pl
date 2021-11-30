01f40c028e54188b2a789be70ecc1884eb38514c
Tiago <32275639+carvalhotiago@users.noreply.github.com>
2021-11-29 21:20:29 -0300

    Adicionando mais alunos, disciplinas e ajustando consulta

--- trabalhoArthurTiago.pl
+++ trabalhoArthurTiago.pl
@@ -1,6 +1,6 @@
 %Trabalho 1 - Linguagem de Programacao
 %Arthur Boechat Mazzi - 201765012C
-%
+%Tiago Carvalho - 201665118C
 
 %Grade de Ciencia da Computacao
 compoeGradeDoCurso("Computacao", "Algoritmos").
@@ -8,13 +8,24 @@ compoeGradeDoCurso("Computacao", "Sistemas Operacionais").
 compoeGradeDoCurso("Computacao", "Teoria dos Grafos").
 compoeGradeDoCurso("Computacao", "Redes de Computadores").
 compoeGradeDoCurso("Computacao", "Estrutura de Dados").
+compoeGradeDoCurso("Computacao", "Estrutura de Dados II").
+compoeGradeDoCurso("Computacao", "Teoria dos Compiladores").
+compoeGradeDoCurso("Computacao", "Linguagens Formais e Automatos").
+compoeGradeDoCurso("Computacao", "Teoria da Computacao").
+compoeGradeDoCurso("Computacao", "Laboratorio de Fisica").
 
 %Grade de Sistemas de Informacao (SI)
 compoeGradeDoCurso("SI", "Algoritmos").
 compoeGradeDoCurso("SI", "Sistemas Operacionais").
 compoeGradeDoCurso("SI", "Banco de Dados").
 compoeGradeDoCurso("SI", "Gerencia de Projetos").
+compoeGradeDoCurso("SI", "Dispositivos Moveis").
+compoeGradeDoCurso("SI", "Empreendimentos em Informatica").
 compoeGradeDoCurso("SI", "Teste de Software").
+compoeGradeDoCurso("SI", "Introducao a SI").
+compoeGradeDoCurso("SI", "Estrutura de Dados II").
+compoeGradeDoCurso("SI", "Teoria dos Grafos").
+
 
 %Grade de Matematica
 compoeGradeDoCurso("Matematica", "Calculo I").
@@ -22,6 +33,11 @@ compoeGradeDoCurso("Matematica", "Calculo II").
 compoeGradeDoCurso("Matematica", "Calculo III").
 compoeGradeDoCurso("Matematica", "Algebra Linear").
 compoeGradeDoCurso("Matematica", "EDO").
+compoeGradeDoCurso("Matematica", "Algoritmos").
+compoeGradeDoCurso("Matematica", "Laboratorio de Fisica").
+compoeGradeDoCurso("Matematica", "Fisica I").
+compoeGradeDoCurso("Matematica", "Analise Real").
+compoeGradeDoCurso("Matematica", "Fundamentos de Matematica").
 
 %Alunos Computacao
 aluno("Tiago", "Computacao", 81).
@@ -29,6 +45,11 @@ aluno("Bruno", "Computacao", 80).
 aluno("Alexandre", "Computacao", 60).
 aluno("Debora", "Computacao", 62).
 aluno("Breno", "Computacao", 69).
+aluno("Lorenzo", "Computacao", 49).
+aluno("Heitor", "Computacao", 99).
+aluno("Helena", "Computacao", 79).
+aluno("Alice", "Computacao", 67).
+aluno("Davi", "Computacao", 61).
 
 %Alunos SI
 aluno("Arthur", "SI", 75).
@@ -36,13 +57,23 @@ aluno("Mariana", "SI", 80).
 aluno("Felipe", "SI", 93).
 aluno("Carlos", "SI", 81).
 aluno("Diogo", "SI", 64).
+aluno("Laura", "SI", 54).
+aluno("Gabriel", "SI", 67).
+aluno("Roberto", "SI", 76).
+aluno("Bernardo", "SI", 91).
+aluno("Pedro", "SI", 36).
 
 %Alunos Matematica
 aluno("Lucas", "Matematica", 88).
 aluno("Vanessa", "Matematica", 77).
 aluno("Amanda", "Matematica", 90).
-aluno("Ana Beatriz", "Matematica", 67).
+aluno("Beatriz", "Matematica", 67).
 aluno("Casimiro", "Matematica", 95).
+aluno("Miguel", "Matematica", 94).
+aluno("Lorena", "Matematica", 65).
+aluno("Manuela", "Matematica", 72).
+aluno("Cecilia", "Matematica", 23).
+aluno("Isabela", "Matematica", 87).
 
 %Disciplinas cursadas alunos computacao
 alunoCursouDisciplina("Tiago", "Algoritmos", 80).
@@ -55,6 +86,16 @@ alunoCursouDisciplina("Debora", "Sistemas Operacionais", 75).
 alunoCursouDisciplina("Debora", "Redes de Computadores", 62).
 alunoCursouDisciplina("Breno", "Algoritmos", 95).
 alunoCursouDisciplina("Breno", "Redes de Computadores", 87).
+alunoCursouDisciplina("Lorenzo", "LFA", 10).
+alunoCursouDisciplina("Lorenzo", "Redes de Computadores", 57).
+alunoCursouDisciplina("Heitor", "LFA", 10).
+alunoCursouDisciplina("Heitor", "Redes de Computadores", 57).
+alunoCursouDisciplina("Helena", "LFA", 10).
+alunoCursouDisciplina("Helena", "Redes de Computadores", 57).
+alunoCursouDisciplina("Alice", "LFA", 10).
+alunoCursouDisciplina("Alice", "Redes de Computadores", 57).
+alunoCursouDisciplina("Davi", "LFA", 10).
+alunoCursouDisciplina("Davi", "Redes de Computadores", 57).
 
 %Disciplinas cursadas alunos SI
 alunoCursouDisciplina("Arthur", "Algoritmos", 70).
@@ -67,6 +108,16 @@ alunoCursouDisciplina("Carlos", "Sistemas Operacionais", 60).
 alunoCursouDisciplina("Carlos", "Bando de Dados", 60).
 alunoCursouDisciplina("Diogo", "Algoritmos",  80).
 alunoCursouDisciplina("Diogo", "Teste de Software", 88).
+alunoCursouDisciplina("Laura", "Algoritmos",  80).
+alunoCursouDisciplina("Laura", "Teste de Software", 88).
+alunoCursouDisciplina("Gabriel", "Algoritmos",  80).
+alunoCursouDisciplina("Gabriel", "Teste de Software", 88).
+alunoCursouDisciplina("Roberto", "Algoritmos",  80).
+alunoCursouDisciplina("Roberto", "Teste de Software", 88).
+alunoCursouDisciplina("Bernardo", "Algoritmos",  80).
+alunoCursouDisciplina("Bernardo", "Teste de Software", 88).
+alunoCursouDisciplina("Pedro", "Algoritmos",  80).
+alunoCursouDisciplina("Pedro", "Teste de Software", 88).
 
 %Disciplinas cursadas alunos Matematica
 alunoCursouDisciplina("Lucas", "Calculo I", 60).
@@ -75,14 +126,24 @@ alunoCursouDisciplina("Vanessa", "EDO", 75).
 alunoCursouDisciplina("Vanessa", "Calculo II", 91).
 alunoCursouDisciplina("Amanda", "Calculo III",  93).
 alunoCursouDisciplina("Amanda", "Algebra Linear", 80).
-alunoCursouDisciplina("Ana Beatriz", "EDO", 67).
-alunoCursouDisciplina("Ana Beatriz", "Calculo I", 90).
+alunoCursouDisciplina("Beatriz", "EDO", 67).
+alunoCursouDisciplina("Beatriz", "Calculo I", 90).
 alunoCursouDisciplina("Casimiro", "Calculo III",  100).
 alunoCursouDisciplina("Casimiro", "Algebra Linear", 100).
+alunoCursouDisciplina("Miguel", "Calculo III",  100).
+alunoCursouDisciplina("Miguel", "Algebra Linear", 100).
+alunoCursouDisciplina("Lorena", "Calculo III",  100).
+alunoCursouDisciplina("Lorena", "Algebra Linear", 100).
+alunoCursouDisciplina("Manuela", "Calculo III",  100).
+alunoCursouDisciplina("Manuela", "Algebra Linear", 100).
+alunoCursouDisciplina("Cecilia", "Calculo III",  100).
+alunoCursouDisciplina("Cecilia", "Algebra Linear", 100).
+alunoCursouDisciplina("Isabela", "Calculo III",  100).
+alunoCursouDisciplina("Isabela", "Algebra Linear", 100).
 
 
 matrizCurricular(X, Z) :- compoeGradeDoCurso(X, Z).
-historicoAluno(X, Z) :- alunoCursouDisciplina(X, Z).
+historicoAluno(X, Z) :- alunoCursouDisciplina(X, Z, _).
 alunosQueCursaramDisciplinaComFiltroNota(X, Z, y) :- alunoCursouDisciplina(Z, X, y).
 estudantesDoCursoComFiltroIRA(X, Z, y) :- aluno(Z, X, y).
 cursosQueContemDisciplina(X, Z) :- compoeGradeDoCurso(Z, X).
